463824e5615a60ed86ff3839c19d27e3116bc972
Alan Koshy <alan.bk@inapp.com>
2020-05-19 18:33:19 +0530

    Added new Excel Package

---
+++ src/main/java/generator/awards/Excel/ExcelController.java
@@ -0,0 +1,8 @@
+package generator.awards.Excel;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExcelController {
+
+}
---
+++ src/main/java/generator/awards/Excel/ExcelService.java
@@ -0,0 +1,8 @@
+package generator.awards.Excel;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelService {
+
+}
